dcc62a6e9969cb7f9de725bbef107878eb4cbf41
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-29 08:37:32 -0700

    generate additional folders and files for app layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-import { createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 
 
