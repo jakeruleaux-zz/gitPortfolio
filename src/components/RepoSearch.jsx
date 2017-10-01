43929b1044c6ecd2e81c54cfa00830507b4af7d2
jacob ruleaux <jakeruleaux@gmail.com>
2017-10-01 12:35:31 -0700

    add submit function to repo search comp

--- src/components/RepoSearch.jsx
+++ src/components/RepoSearch.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getRepo } from './../actions';
 
 class RepoSearch extends React.Component {
 
   constructor(props) {
     super(props);
+    this.handleSubmitSearch = this.handleSubmitSearch.bind(this);
+  }
 
+  handleSubmitSearch(event) {
+    event.preventDefault();
+    let { _name } = this.refs;
+    if (!_name.value.trim()) {
+      return;
+    }
+    this.props.dispatch(getRepoId(_name.value.trim()); _name.value = " ";
   }
 
   render() {
     return(
       <div>
-        <form>
-          <input></input>
+        <form onSubmit={this.handleSubmitSearch}>
+          <input placeholder="name" ref="_name"></input>
           <button>btn</button>
         </form>
       </div>
